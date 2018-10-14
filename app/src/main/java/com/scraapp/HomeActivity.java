1fe7352a1f187d05e9c1ab9d81f9e851e84ba9fd
AristoM <aristo.mca@gmail.com>
2018-10-15 00:34:59 +0530

    minor updates

--- app/src/main/java/com/scraapp/HomeActivity.java
+++ app/src/main/java/com/scraapp/HomeActivity.java
@@ -55,29 +55,29 @@ public class HomeActivity extends AppCompatActivity {
 
     private void performSearch() {
 
-        List<Address> addresses = geoCoder.getFromLocationName(mPickupLocation.getText().toString(),5);
-
-        if(addresses.size() > 0) {
-            p = new GeoPoint( (int) (addresses.get(0).getLatitude() * 1E6),
-                    (int) (addresses.get(0).getLongitude() * 1E6));
-
-            controller.animateTo(p);
-            controller.setZoom(12);
-
-            MapOverlay mapOverlay = new MapOverlay();
-            List<Overlay> listOfOverlays = map.getOverlays();
-            listOfOverlays.clear();
-            listOfOverlays.add(mapOverlay);
-
-            map.invalidate();
-            txtsearch.setText("");
-        }
-        else {
-            AlertDialog.Builder adb = new AlertDialog.Builder(HomeActivity.this);
-            adb.setTitle("Google Map");
-            adb.setMessage("Please Provide the Proper Place");
-            adb.setPositiveButton("Close",null);
-            adb.show();
-        }
+//        List<Address> addresses = geoCoder.getFromLocationName(mPickupLocation.getText().toString(),5);
+//
+//        if(addresses.size() > 0) {
+//            p = new GeoPoint( (int) (addresses.get(0).getLatitude() * 1E6),
+//                    (int) (addresses.get(0).getLongitude() * 1E6));
+//
+//            controller.animateTo(p);
+//            controller.setZoom(12);
+//
+//            MapOverlay mapOverlay = new MapOverlay();
+//            List<Overlay> listOfOverlays = map.getOverlays();
+//            listOfOverlays.clear();
+//            listOfOverlays.add(mapOverlay);
+//
+//            map.invalidate();
+//            txtsearch.setText("");
+//        }
+//        else {
+//            AlertDialog.Builder adb = new AlertDialog.Builder(HomeActivity.this);
+//            adb.setTitle("Google Map");
+//            adb.setMessage("Please Provide the Proper Place");
+//            adb.setPositiveButton("Close",null);
+//            adb.show();
+//        }
     }
 }
--- app/src/main/java/com/scraapp/MapOverlay.java
+++ app/src/main/java/com/scraapp/MapOverlay.java
@@ -7,21 +7,22 @@ import android.graphics.Point;
 
 import com.google.android.gms.maps.MapView;
 
-class MapOverlay extends com.google.android.maps.Overlay
-{
-    public boolean draw(Canvas canvas, MapView mapView,
-                        boolean shadow, long when)
-    {
-        super.draw(canvas, mapView, shadow);
-
-        //---translate the GeoPoint to screen pixels---
-        Point screenPts = new Point();
-        mapView.getProjection().toPixels(p, screenPts);
-
-        //---add the marker---
-        Bitmap bmp = BitmapFactory.decodeResource(
-                getResources(), R.drawable.current_location);
-        canvas.drawBitmap(bmp, screenPts.x, screenPts.y-32, null);
-        return true;
-    }
+class MapOverlay {
+//        extends com.google.android.maps.Overlay
+//{
+//    public boolean draw(Canvas canvas, MapView mapView,
+//                        boolean shadow, long when)
+//    {
+//        super.draw(canvas, mapView, shadow);
+//
+//        //---translate the GeoPoint to screen pixels---
+//        Point screenPts = new Point();
+//        mapView.getProjection().toPixels(p, screenPts);
+//
+//        //---add the marker---
+//        Bitmap bmp = BitmapFactory.decodeResource(
+//                getResources(), R.drawable.current_location);
+//        canvas.drawBitmap(bmp, screenPts.x, screenPts.y-32, null);
+//        return true;
+//    }
 }
